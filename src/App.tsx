93a85646a5762a6c8b8ca81148f87af800ab2bf8
Andrei Adnolka <andrei.adnolka@gmail.com>
2024-06-24 22:08:58 +0300

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,22 @@
-import Board from "./components/board";
-import UpcomingBlocks from "./components/upcoming-blocks";
-import ButtonsUI from "./components/buttons";
-import { useTetris } from "./hooks/useTetris";
-
-import { PauseSvg } from "./svg/pause";
-import { ContinueSvg } from "./svg/continue";
+import Tetris from "./module/tetris";
+import { Route, Routes } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 function App() {
-  const {
-    board,
-    startGame,
-    pauseGame,
-    isPlaying,
-    isPause,
-    isFinished,
-    isStart,
-    score,
-    upcomingBlocks,
-    level,
-  } = useTetris();
-
-  const isStoppedGame = isPlaying || isPause;
+  const { pathname } = useLocation();
+  const isLoadedGame = pathname !== "/";
 
   return (
     <div className="app">
-      <h1>TETRIS</h1>
-      <div className="elements">
-        <Board
-          currentBoard={board}
-          isPaused={!isPlaying || isPause || isFinished}
-        />
-        {isFinished ? (
-          <div className="popup game_over_popup">GAME OVER</div>
-        ) : null}
-        {isPause ? (
-          <div className="popup" onClick={pauseGame}>
-            PAUSED
-          </div>
-        ) : null}
-        {isPlaying ? null : (
-          <div onClick={startGame} className="start_game popup">
-            {isPause ? "NEW GAME" : "START"}
-          </div>
-        )}
-        <div className="controls">
-          <div className="info_block">
-            <div>SCORE</div>
-            <div>{score}</div>
-          </div>
-          <div className="info_block">
-            <div>LEVEL</div>
-            <div>{level}</div>
-          </div>
-          <div className="new_game_with_upcoming">
-            {isStoppedGame ? (
-              <UpcomingBlocks upcomingBlocks={upcomingBlocks} />
-            ) : null}
-          </div>
-          <div className="pause_button">
-            {isStart && !isFinished ? (
-              <div onClick={pauseGame} id="paused">
-                {isPause ? <ContinueSvg /> : <PauseSvg />}
-              </div>
-            ) : null}
-          </div>
+      {isLoadedGame ? null : (
+        <div className="home_page">
+          <h2>MENU</h2>
+          <a href="/tetris">TETRIS</a>
         </div>
-      </div>
-      <ButtonsUI />
+      )}
+      <Routes>
+        <Route path="/tetris" element={<Tetris />} />
+      </Routes>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.scss";
+import { BrowserRouter } from "react-router-dom";
+
 import App from "./App";
 import { TelegramProvider } from "./provider/telegram";
+import "./index.scss";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -10,7 +12,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <TelegramProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </TelegramProvider>
   </React.StrictMode>
 );
