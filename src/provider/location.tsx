d22d69cc014888552367ae05f3e12f33351cf014
Andrei Adnolka <andrei.adnolka@gmail.com>
2024-06-28 23:32:26 +0300

    check 9

--- src/provider/location.tsx
+++ src/provider/location.tsx
@@ -6,37 +6,37 @@ interface LocationProviderProps {
   children: React.ReactNode;
 }
 
-export const LocationProvider: React.FC<LocationProviderProps> = ({ children }) => {
+export const LocationProvider: React.FC<LocationProviderProps> = ({
+  children,
+}) => {
   // @ts-ignore
   if (!window?.Telegram?.WebApp?.initData?.length) {
-    return <>{children}</>
+    return <>{children}</>;
   } else {
-    return <LocationProviderInner>{children}</LocationProviderInner>
+    return <LocationProviderInner>{children}</LocationProviderInner>;
   }
-}
+};
 
 const LocationProviderInner: React.FC<LocationProviderProps> = ({
   children,
 }) => {
-  const location = useLocation()
-  const navigate = useNavigate()
+  const location = useLocation();
+  const navigate = useNavigate();
 
   const redirectToLastPage = () => {
-    const currentLocation = Cookies.get("location_app")
-    if (!currentLocation) return
-    navigate(currentLocation)
-  }
+    const currentLocation = Cookies.get("location_app");
+    if (!currentLocation) return;
+    navigate(currentLocation);
+  };
 
-  React.useEffect(() => {
-    if (location.key === "default") redirectToLastPage()
+  useEffect(() => {
+    if (location.key === "default") redirectToLastPage();
 
     Cookies.set("location_app", location.pathname, {
       expires: new Date(new Date().getTime() + 10 * 60 * 1000), // 10 min
-    })
-  }, [location.key])
-
-  return <>{children}</>
-}
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location.key]);
 
   return <>{children}</>;
 };
