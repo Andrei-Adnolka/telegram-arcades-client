1c8f70e5d13a0f2dfcc1c79dc56712c2f9e23026
Andrei Adnolka <andrei.adnolka@gmail.com>
2024-06-28 23:34:10 +0300

    check 10

--- src/provider/location.tsx
+++ src/provider/location.tsx
@@ -1,4 +1,4 @@
-import { useEffect, FC, useCallback } from "react";
+import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
 
