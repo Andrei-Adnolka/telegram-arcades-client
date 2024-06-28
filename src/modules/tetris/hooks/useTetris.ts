68da74924a11371772d5a480c2c1e24742834af5
Andrei Adnolka <andrei.adnolka@gmail.com>
2024-06-28 16:34:55 +0300

    updated

--- src/modules/tetris/hooks/useTetris.ts
+++ src/modules/tetris/hooks/useTetris.ts
@@ -137,14 +137,30 @@ export function useTetris() {
     } else {
       dispatchBoardState({ type: "drop" });
     }
+    setItem({
+      board,
+      droppingColumn,
+      droppingBlock,
+      droppingRow,
+      droppingShape,
+      isCommitting,
+      score,
+      speed,
+      upcomingBlocks,
+    });
   }, [
     board,
     commitPosition,
     dispatchBoardState,
+    droppingBlock,
     droppingColumn,
     droppingRow,
     droppingShape,
     isCommitting,
+    score,
+    setItem,
+    speed,
+    upcomingBlocks,
   ]);
 
   useInterval(() => {
