ccc78ea4d3db607b1392917ed03133f7d6be1b44
Andrei Adnolka <andrei.adnolka@gmail.com>
2024-07-22 17:45:06 +0300

    check v3

--- app/[[...slug]]/client.tsx
+++ app/[[...slug]]/client.tsx
@@ -4,15 +4,18 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import dynamic from "next/dynamic";
 import { TelegramProvider } from "src/provider/telegram";
+import { useMounted } from "src/hooks/useMounted";
 
 const App = dynamic(() => import("../../src/App"), { ssr: false });
 
 export function ClientOnly() {
-  return (
+  const mounted = useMounted();
+
+  return mounted ? (
     <TelegramProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </TelegramProvider>
-  );
+  ) : null;
 }
