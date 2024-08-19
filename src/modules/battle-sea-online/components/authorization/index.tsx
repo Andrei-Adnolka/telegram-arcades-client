7632522fbbd04a955a3d9b8e8488293aa28c5888
Andrei Adnolka <andrei.adnolka@gmail.com>
2024-08-19 23:01:20 +0300

    updated

--- src/modules/battle-sea-online/components/authorization/index.tsx
+++ src/modules/battle-sea-online/components/authorization/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import NicknameField from "./components/nickname-field";
